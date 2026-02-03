fa505496ac20afb0b487e4e14b026f66be7135f1
Ben McDonald <ben.mcdonald@hpe.com>
2026-02-03 14:44:37 -0800

    Fix reconciler naming

--- pkg/reconcilers/node_reconciler.go
+++ pkg/reconcilers/node_reconciler.go
@@ -2,72 +2,63 @@ package reconcilers
 
 import (
 	"context"
-	"time"
 
-	"github.com/openchami/fabrica/pkg/reconcile"
 	"github.com/OpenCHAMI/node-service/pkg/clients"
 	"github.com/OpenCHAMI/node-service/pkg/resources/node"
 )
 
-type NodeReconciler struct {
-	*NodeReconcilerGenerated // Embedding the generated boilerplate
-    Clients *clients.ServiceClients
-}
+// NOTE: Do not define type NodeReconciler struct here.
+// It is already defined in node_reconciler_generated.go.
 
-func (r *NodeReconciler) ReconcileNode(ctx context.Context, res *node.Node) (reconcile.Result, error) {
-	if r.Clients == nil {
-        // Hardcoded URLs for now, or fetch from env vars
-        r.Clients = clients.NewServiceClients("http://localhost:8081", "http://localhost:8082")
-    }
-	// This function is the "Brain" of the Node resource.
-    // It runs whenever a Node is created, updated, or the requeue timer expires.
+// reconcileNode is the "Brain" of the Node resource.
+// The generated Reconcile() wrapper calls this function.
+func (r *NodeReconciler) reconcileNode(ctx context.Context, res *node.Node) error {
+	// Initialize the client locally since we can't add fields to the struct
+	// In production, we might use a singleton pattern or environment variables
+	serviceClients := clients.NewServiceClients("http://localhost:8081", "http://localhost:8082")
 
 	// 1. Fetch External Data
-    // We use the xname to look up data in the other services
-    bootInfo, err := r.Clients.FetchBootConfig(res.Spec.XName)
-    if err != nil {
-        // If we can't talk to boot-service, we log it but don't crash
-        // We might want to requeue quickly to retry
-        return reconcile.Result{RequeueAfter: 30 * time.Second}, err
-    }
+	// We use the xname to look up data in the other services
+	bootInfo, err := serviceClients.FetchBootConfig(res.Spec.XName)
+	if err != nil {
+		// Returning an error here triggers the generated code to:
+		// 1. Log the error
+		// 2. Set Status "Ready" = "False"
+		// 3. Requeue after 30 seconds
+		return err
+	}
 
-    metaInfo, err := r.Clients.FetchMetaConfig(res.Spec.XName)
-    if err != nil {
-        return reconcile.Result{RequeueAfter: 30 * time.Second}, err
-    }
+	metaInfo, err := serviceClients.FetchMetaConfig(res.Spec.XName)
+	if err != nil {
+		return err
+	}
 
-    // 2. Update Status (The Composed View)
-    // We compare current status with new data to avoid unnecessary writes
-    changed := false
+	// 2. Update Status (The Composed View)
+	changed := false
 
-    // Update Boot Status
-    if res.Status.Boot.Kernel != bootInfo.Kernel {
-        res.Status.Boot.Kernel = bootInfo.Kernel
-        res.Status.Boot.Params = bootInfo.Params
-        res.Status.Boot.Image = bootInfo.Image
-        changed = true
-    }
+	// Update Boot Status
+	if res.Status.Boot.Kernel != bootInfo.Kernel {
+		res.Status.Boot.Kernel = bootInfo.Kernel
+		res.Status.Boot.Params = bootInfo.Params
+		res.Status.Boot.Image = bootInfo.Image
+		changed = true
+	}
 
-    // Update Config Status
-    if len(res.Status.Config.Groups) != len(metaInfo.Groups) {
-        res.Status.Config.Groups = metaInfo.Groups
-        changed = true
-    }
-    // (Note: deeper slice comparison would be better for production)
+	// Update Config Status
+	if len(res.Status.Config.Groups) != len(metaInfo.Groups) {
+		res.Status.Config.Groups = metaInfo.Groups
+		changed = true
+	}
 
-    // 3. Save if changed
-    if changed {
-        // Determine effective profile (Phase 4 placeholder)
-        if res.Status.EffectiveProfile == "" {
-            res.Status.EffectiveProfile = "default"
-        }
-        
-        // The generated wrapper handles the actual .Save() call if we modify the object
-        // but we explicitly return it here to be safe/clear
-    }
+	// 3. Save Logic
+	if changed {
+		if res.Status.EffectiveProfile == "" {
+			res.Status.EffectiveProfile = "default"
+		}
+		// We don't need to call Save() manually. 
+		// The generated wrapper detects that we modified 'res' and will save it for us
+		// if we return nil.
+	}
 
-	// 4. Periodic Sync
-    // We want to poll frequently because the boot-service might change 
-    // without us knowing.
-	return reconcile.Result{RequeueAfter: 1 * time.Minute}, nil
+	return nil
 }
